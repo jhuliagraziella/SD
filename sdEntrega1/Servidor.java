b3b7a2c778a2f8c4086a54e017b0b56ff22a8869
Jhúlia Graziella <jhuliagraziella@gmail.com>
2017-05-31 01:04:35 -0300

    Add files via upload

---
+++ sdEntrega1/Servidor.java
@@ -0,0 +1,32 @@
+package sdEntrega1;
+ 
+import sdEntrega1.*;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.transport.TServerTransport;
+import org.apache.thrift.server.TServer.Args;
+import org.apache.thrift.server.TSimpleServer;
+import org.apache.thrift.server.TThreadPoolServer;
+ 
+ 
+public class Servidor {
+    public static Grafo handler;
+    public static sdEntrega1.Processor processor;
+
+    public static void main(String [] args) {
+        try {
+            handler = new Grafo();
+            processor = new sdEntrega1.Processor(handler);
+ 
+            TServerTransport serverTransport = new TServerSocket(9090);
+            TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
+ 
+            System.out.println("Iniciando...");
+            server.serve();
+ 
+        }
+        catch (Exception x) {
+            x.printStackTrace();
+        }
+    }
+}
---
+++ sdEntrega1/Vertice.java
@@ -0,0 +1,53 @@
+package sdEntrega1;
+
+public class Vertice {
+    int id;
+    int cor;
+    double peso;
+    String descricao;
+
+    public Vertice(int id, int cor, double peso, String descricao){
+        this.id = id;
+        this.cor = cor;
+        this.peso = peso;
+        this.descricao = descricao;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public int getCor(){
+        return this.cor;
+    }
+
+    public double getPeso(){
+        return this.peso;
+    }
+
+    public String getDescricao(){
+        return this.descricao;
+    }
+
+    public String getInformacoes(){
+        String s = "";
+        s += "Nome: " + String.valueOf(this.id) + "\n";
+        s += "Cor: " + String.valueOf(this.cor) + "\n";
+        s += "Peso: " + String.valueOf(this.peso) + "\n";
+        s += "Descricao: " + this.descricao + "\n";
+    
+        return s;
+    }
+
+    public void setCor(int cor){
+        this.cor = cor;
+    }
+
+    public void setPeso(double peso){
+        this.peso = peso;
+    }
+
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+}
